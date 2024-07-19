06209ffdf855db6aab54ddeed7e86d4a41333fec
toshiaki saeki <toshiaki61@gmail.com>
2024-07-19 21:06:38 +0900

    test: 💍 filelist

--- tests/test.ts
+++ tests/test.ts
@@ -3,4 +3,5 @@ import { EventEmitter } from 'node:events'
 
 const ee = new EventEmitter()
 runFromFile('./tests/basic.yml').then(({ result }) => console.log(result.tests[0].steps))
+runFromFile('./tests/filelist.yml').then(({ result }) => console.log(result.tests[0].steps))
 runFromFile('./tests/multipart.yml').then(({ result }) => console.log(result.tests[0].steps))
